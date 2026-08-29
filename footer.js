17508958941a2999bcbfd8f5f4a8a13fcf23b030
vjp-tech <verenapolzhofer@gmail.com>
2026-08-29 13:09:29 +0200

    Update

--- footer.js
+++ footer.js
@@ -112,7 +112,7 @@ const sfFooterHTML = `
         </svg>
       </a>
       <a class="sf-social-link"
-         href="https://t.me/sinnfunk"
+         href="https://t.me/sinnfunk_webradio"
          target="_blank" rel="noopener" aria-label="Sinnfunk auf Telegram">
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
           <path d="M22 2 11 13"/>
